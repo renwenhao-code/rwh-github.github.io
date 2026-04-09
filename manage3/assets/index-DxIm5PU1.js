f82a271d4acceaaeeaf4c500f03d526f68d36325
renwenhao-code <130143611+renwenhao-code@users.noreply.github.com>
2026-04-09 14:17:32 +0800

    Add files via upload

---
+++ manage3/assets/index-DxIm5PU1.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as n,c as t,b as c}from"./index-ByvsWdm-.js";const s={};function _(a,i){const e=r("router-view");return n(),t("div",null,[c(e)])}const d=o(s,[["render",_]]);export{d as default};
